c418e087d884d46e8e274da43701f24d6085369e
Vincent <booncheng89@gmail.com>
2021-03-04 22:32:58 +0800

    Port over from RN Digital Wallet to Expo version

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React from 'react';
+
+import { SignUp } from "./screens";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import Tabs from "./navigation/tabs";
+
+const theme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        border: "transparent",
+    },
+};
+
+const Stack = createStackNavigator();
+
+const App = () => {
+    const [loaded] = useFonts({
+        "Roboto-Black" : require('./assets/fonts/Roboto-Black.ttf'),
+        "Roboto-Bold" : require('./assets/fonts/Roboto-Bold.ttf'),
+        "Roboto-Regular" : require('./assets/fonts/Roboto-Regular.ttf'),
+    })
+    
+    if(!loaded){
+    return null;
+    }
+    return (
+        <NavigationContainer theme={theme}>
+            <Stack.Navigator
+                screenOptions={{
+                    headerShown: false
+                }}
+                initialRouteName={'SignUp'}
+            >
+                <Stack.Screen name="SignUp" component={SignUp} />
+
+                {/* Tabs */}
+                <Stack.Screen name="Home" component={Tabs} />
+
+                {/* <Stack.Screen name="Scan" component={Scan} /> */}
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
